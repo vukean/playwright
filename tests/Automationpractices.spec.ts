3c5dc0ea02b7a34c4f7cd6c65492b384c491debf
Vu Ke An <anvuke2001@gmail.com>
2026-04-05 14:07:25 +0700

    Update Playwright test files

--- tests/Automationpractices.spec.ts
+++ tests/Automationpractices.spec.ts
@@ -1,1 +1,13 @@
 import { test, expect, request } from '@playwright/test'
+import { getDefaultSettings } from 'node:http2'
+hehehehehe 
+asdasd'
+fsdg
+getDefaultSettings
+dsfgdsfg
+dfgsdfg
+
+getDefaultSettingsdsfg
+
+getDefaultSettings
+gdfsgdsfgdfsgsdf
