718124659249d618278da3128c90ddc1a509b878
Vu Ke An <anvuke2001@gmail.com>
2026-04-05 15:30:20 +0700

    Them login page

--- tests/Automationpractices.spec.ts
+++ tests/Automationpractices.spec.ts
@@ -4,11 +4,10 @@ test('Test input Field', async ({ page }) => {
   await page.goto('https://practice.expandtesting.com/inputs');
 
   // Input Number
-
   const numberInput = page.getByRole('spinbutton', { name: 'Input: Number' })
   await numberInput.fill('100');
   await expect(numberInput).toHaveValue('100');
-  
+
     // Input Number click arrowup + check each time press arrowUp
     const numberArrowUp =page.getByRole('spinbutton', { name: 'Input: Number' })
     await numberArrowUp.press('ArrowUp');
@@ -45,4 +44,17 @@ test('Test input Field', async ({ page }) => {
   const dateInput = page.getByRole('textbox', { name: 'Input: Date' })
   await dateInput.fill('2025-04-10');
   await expect(dateInput).toHaveValue('2025-04-10');
+});
+test('Login page',async({page})=>{
+  await page.goto('https://practice.expandtesting.com/login');
+  const userName = page.getByRole('textbox', { name: 'Username' })
+  const passWord = page.getByRole('textbox', { name: 'Password' })
+  await userName.fill('practice')
+  await expect(userName).toHaveValue('practice')
+  await passWord.fill('SuperSecretPassword!')
+  await expect(passWord).toHaveValue('SuperSecretPassword!')
+  const btnLogin = page.getByRole('button', { name: 'Login' })
+  await btnLogin.click();
+  expect 
+  
 });
